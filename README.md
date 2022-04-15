317a109ae162ed0c95b0480ff30d5d35ec412cdf
Mahendra Liya <liyamahendra4@gmail.com>
2022-04-15 11:45:51 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,9 @@
+# cordova-plugin-imagepicker
+Cordova Image Picker plugin for Android & iOS
+
+# Description
+
+The plugin allows you to Pick an image from the Gallery. It also allows to Crop and Compresses the Image based on Aspect Ratio, Resolution and Image Size.
+
+# Installation
+
---
+++ src/ios/ImagePicker.m
@@ -0,0 +1,28 @@
+/********* ImagePicker.m Cordova Plugin Implementation *******/
+
+#import <Cordova/CDV.h>
+
+@interface ImagePicker : CDVPlugin {
+  // Member variables go here.
+}
+
+- (void)coolMethod:(CDVInvokedUrlCommand*)command;
+@end
+
+@implementation ImagePicker
+
+- (void)coolMethod:(CDVInvokedUrlCommand*)command
+{
+    CDVPluginResult* pluginResult = nil;
+    NSString* echo = [command.arguments objectAtIndex:0];
+
+    if (echo != nil && [echo length] > 0) {
+        pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_OK messageAsString:echo];
+    } else {
+        pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_ERROR];
+    }
+
+    [self.commandDelegate sendPluginResult:pluginResult callbackId:command.callbackId];
+}
+
+@end
