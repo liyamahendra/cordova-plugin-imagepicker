017c10d9b146469023a9e45d948f41343b51e432
Mahendra Liya <liyamahendra4@gmail.com>
2022-04-15 12:29:26 +0530

    Update plugins.xml

--- www/ImagePicker.js
+++ www/ImagePicker.js
@@ -3,3 +3,19 @@ var exec = require('cordova/exec');
 exports.coolMethod = function (arg0, success, error) {
     exec(success, error, 'ImagePicker', 'coolMethod', [arg0]);
 };
+
+exports.validateOutputType = function (success, error, options) {
+    exec(success, error, 'ImagePicker', 'validateOutputType', [options]);
+};
+
+exports.hasReadPermission = function (success, error) {
+    exec(success, error, 'ImagePicker', 'hasReadPermission');
+};
+
+exports.requestReadPermission = function (success, error) {
+    exec(success, error, 'ImagePicker', 'requestReadPermission');
+};
+
+exports.getPictures = function (success, error, options) {
+    exec(success, error, 'ImagePicker', 'getPictures', [options]);
+};
